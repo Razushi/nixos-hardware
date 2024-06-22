4631b9e83c8deb9f24a86bc8400b02cac82a03a5
Jörg Thalheim <joerg@thalheim.io>
2024-06-22 08:42:43 +0200

    common/gpu/24.05-compat: don't create conflicts with user configuration
    fixes https://github.com/NixOS/nixos-hardware/issues/1000

--- common/gpu/24.05-compat.nix
+++ common/gpu/24.05-compat.nix
@@ -8,30 +8,30 @@
   options = {
     hardware.graphics = lib.optionalAttrs (lib.versionOlder lib.version "24.11pre") {
       enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
+        type = lib.types.nullOr lib.types.bool;
+        default = null;
       };
       enable32Bit = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
+        type = lib.types.nullOr lib.types.bool;
+        default = null;
       };
       extraPackages = lib.mkOption {
-        type = lib.types.listOf lib.types.package;
-        default = [];
+        type = lib.types.nullOr (lib.types.listOf lib.types.package);
+        default = null;
       };
       extraPackages32 = lib.mkOption {
-        type = lib.types.listOf lib.types.package;
-        default = [];
+        type = lib.types.nullOr (lib.types.listOf lib.types.package);
+        default = null;
       };
     };
   };
 
   config = {
     hardware.opengl = lib.optionalAttrs (lib.versionOlder lib.version "24.11pre") {
-      enable = config.hardware.graphics.enable;
-      driSupport32Bit = config.hardware.graphics.enable32Bit;
-      extraPackages = config.hardware.graphics.extraPackages;
-      extraPackages32 = config.hardware.graphics.extraPackages32;
+      enable = lib.mkIf (config.hardware.graphics != null) config.hardware.graphics.enable;
+      driSupport32Bit = lib.mkIf (config.hardware.graphics.enable32Bit != null) config.hardware.graphics.enable32Bit;
+      extraPackages = lib.mkIf (config.hardware.graphics.extraPackages != null) config.hardware.graphics.extraPackages;
+      extraPackages32 = lib.mkIf (config.hardware.graphics.extraPackages32 != null) config.hardware.graphics.extraPackages32;
     };
   };
 }
